996f562e247080d095691c7dd3c39b47feb177e3
ElsoloxD <76150949+ElsoloxD@users.noreply.github.com>
2021-01-13 22:54:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let fondo = document.getElementsByTagName('body')
+
+fondo[0].style.backgroundColor = '#fcf79f'
+
+let parrafo = document.getElementsByTagName('p')
+
+parrafo[0].style.backgroundColor = '#0ca001'
+parrafo[1].style.backgroundColor = '#0ca001'
+parrafo[2].style.backgroundColor = '#0ca001'
+parrafo[3].style.backgroundColor = '#0ca001'
+parrafo[4].style.backgroundColor = '#0ca001'
+
+let upper = document.getElementById('destacado')
+
+upper.style.fontSize = '24px'
+
+let changeFont = document.getElementsByTagName('h2')
+
+changeFont[0].style.fontFamily = 'Arial'
+changeFont[1].style.fontFamily = 'Arial'
+
+let link = document.getElementById('linking')
+link.innerHTML = '<a href="https://www.lipsum.com">Ir hacia el sitio web lipsum</a>'
